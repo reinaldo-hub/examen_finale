8531c261524dc6ec17b94fa90badafc63eb36d80
Reypo <rey301174@gmail.com>
2020-03-07 14:41:07 -0500

    avancement conference

--- front-page.php
+++ front-page.php
@@ -1,91 +1,32 @@
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package underscores
- */
 
+
+<?php underscores_post_thumbnail(); ?>
+<?php
 get_header();
 ?>
-    <div class="¨bob">
-        <p class="gg">dsdsds</p>
-    </div>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-		<?php
-		while ( have_posts() ) :
-			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-        <h2>Conférences</h2>
-        <div class="conference">
+    <h2 class="container">Nos dernieres nouvelles</h2>
+    <div class="conference container">
+    <?php
+        $args = array(
+        'category_name' => 'conference',
+        'posts_per_page' => 3
+        );
         
-            <?php
+        $query1 = new WP_Query($args);
 
-               $args = array(
-                'category_name' => 'evenement',
-                'posts_per_page' => 4,
-                'order' => 'DESC',
-                );
-                
-                $query1 = new WP_Query($args);
-
-                while($query1->have_posts()):
-                    
-                    $query1->the_post();
-
-                    echo "<div>";
-                        the_post_thumbnail();
-                        echo "<h4>" . get_the_title() . "</h4>";
-                        echo "<p>" . get_the_excerpt() . "</p>";
-                    echo "</div>";
-
-                endwhile;
-
+        while($query1->have_posts()):
             
-            ?>
+            $query1->the_post();
 
-        </div>
+            echo "<div>";
+                the_post_thumbnail();
+                echo "<h4>" . get_the_title() . "</h4>";
+                echo "<p>" . get_the_excerpt() . "</p>";
+            echo "</div>";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+        endwhile;
+    ?>
+    </div>
+    </main><!-- #main -->
+</div><!-- #primary -->
